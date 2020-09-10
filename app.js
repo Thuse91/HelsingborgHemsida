5488edd30d2de36b1337e0e1710c876752e71929
nigg <thuse91@live.se>
2020-09-10 17:54:28 +0200

    lagt till massa content men mycket kvar, under 6h sedan start

---
+++ app.js
@@ -0,0 +1,30 @@
+const img = document.getElementById('carousel');
+const rightBtn = document.getElementById('right-btn');
+const leftBtn = document.getElementById('left-btn');
+
+let pictures = 
+    img.src = pictures[0];
+let position = 0;
+
+moveRight = () => {
+    if (position >= pictures.length - 1) {
+        position = 0
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position + 1];
+    position++;
+}
+
+moveLeft = () => {
+    if (position < 1) {
+        position = pictures.length - 1;
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position - 1];
+    position--;
+}
+
+rightBtn.addEventListener("click", moveRight);
+leftBtn.addEventListener("click", moveLeft);
